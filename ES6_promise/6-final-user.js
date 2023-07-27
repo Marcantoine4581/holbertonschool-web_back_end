008afa67b8812749ff6079adce3f4e77bf15ad2a
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-07-27 15:51:57 +0000

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName),
     uploadPhoto(fileName)];
 
-  return Promise.allSettled(promises);
+  return Promise.allSettled(promises)
+    .then((results) => {
+      const array = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          array.push(result);
+        } else {
+          array.push({
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          });
+        }
+      });
+      return array;
+    });
 }
