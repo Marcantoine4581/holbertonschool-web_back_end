4085fc1278ae13a0c9e5a61b2124721cb9699e23
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-07-28 09:06:16 +0000

    task 100

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  let names = [];
+  for (const value of Object.values(report.allEmployees)) {
+    names = [...names, ...value];
+  }
+  return names;
+}
