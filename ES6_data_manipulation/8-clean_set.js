4fe841a01e21a4b59973b37d462edecfb45f303b
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-07-31 10:36:08 +0000

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (!startString || !startString.length) {
     return '';
   }
   let result = '';
   for (const element of set) {
-    if (element.startsWith(startString)) {
+    if (element && element.startsWith(startString)) {
       if (result.length === 0) {
         result += element.replace(startString, '');
       } else {
