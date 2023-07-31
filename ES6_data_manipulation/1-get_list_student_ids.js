54b4876d1b2902e85e1665eb34a073f1f837a097
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-07-31 06:43:32 +0000

    task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(arrayOfObjects) {
-  if (typeof arrayOfObjects !== 'object') {
+  if (typeof arrayOfObjects !== 'object' || arrayOfObjects === null) {
     return [];
   }
   return arrayOfObjects.map((value) => value.id);
