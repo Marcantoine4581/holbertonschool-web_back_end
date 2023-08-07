d7c21a99d4e46d1cb94782eefcf7f7ea5946506f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-07 15:35:14 +0000

    task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -17,11 +17,7 @@ async def measure_runtime() -> float:
         The total runtime.
     """
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
     end = time.perf_counter()
     return end - start
