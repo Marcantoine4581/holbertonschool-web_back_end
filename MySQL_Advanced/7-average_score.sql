482eb85c7a207b86d9721e244bf8b98139de4347
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-10 06:33:54 +0000

    task 7

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
+BEGIN
+	DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS c WHERE c.user_id = user_id);
+    UPDATE users SET average_score = avg_score where id = user_id;
+END//
+
+DELIMITER ;
