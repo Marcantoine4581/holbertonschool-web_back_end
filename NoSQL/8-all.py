666439b6bec07a3ff7939c87ed1b2b6d138f473f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-14 14:18:47 +0000

    function documented

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,6 +4,13 @@ import pymongo
 
 
 def list_all(mongo_collection):
+    """function that lists all documents in a collection
+    Args:
+        mongo_collection (): a mongo collection
+    Returns:
+        All documents in a collection
+        OR an empty list if no document in the collection
+    """
     if mongo_collection.count() == 0:
         return []
     return mongo_collection.find()
