25f533a1d025e95bdff1f0a237ca7f249cfd3732
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-15 08:26:39 +0000

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document based on the name
+    Args:
+        mongo_collection: pymongo collection object
+        name (str): the school name to update.
+        topics (list of strings): list of topics approached in the school
+    Returns:
+        Nothing.
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {
+            "$set": {"topic": topics}
+        }
+    )
