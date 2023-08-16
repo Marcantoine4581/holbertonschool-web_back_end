cf43d3a4d5e6881ad0d788795fa1b92d91ffbeec
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-16 09:35:18 +0000

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Redis basic'''
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        '''Get the key from Redis'''
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_str(self, string: bytes) -> str:
+        '''get a string from a byte string'''
+        return string.decode("utf-8")
+
+    def get_int(self, number: int) -> int:
+        '''get an integer'''
+        return int(number)
