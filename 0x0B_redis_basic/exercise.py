27a942e742418da8f1d0c7633bcd4a310df71ee8
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-16 10:31:15 +0000

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,8 @@ import redis
 from typing import Union, Optional, Callable
 from uuid import uuid4
 
+UnionOfTypes = Union[str, bytes, int, float]
+
 
 class Cache:
     ''' Cache redis class'''
@@ -12,23 +14,26 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''store data into redis cache'''
-        key = str(uuid4())
-        self._redis.mset({key: data})
-        return key
+    def store(self, data: UnionOfTypes) -> str:
+        '''Store data into redis cache using a random key
+        and returns the key'''
+        random_key = str(uuid4())
+        self._redis.mset({random_key: data})
+        return random_key
 
-    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
         '''Get the key from Redis'''
+        value = self._redis.get(key)
         if fn:
-            return fn(self._redis.get(key))
-        data = self._redis.get(key)
-        return data
+            value = fn(value)
+        return value
 
-    def get_str(self, string: bytes) -> str:
-        '''get a string from a byte string'''
-        return string.decode("utf-8")
+    def get_str(self, key: str) -> str:
+        '''Parameterizes a value from redis to str'''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
 
-    def get_int(self, number: int) -> int:
-        '''get an integer'''
-        return int(number)
+    def get_int(self, key: str) -> int:
+        '''Parameterizes a value from redis to int'''
+        value = self._redis.get(key)
+        return int(value)
