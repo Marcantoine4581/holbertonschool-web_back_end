2d77abc326becb15d3bf937948fe986c5be50725
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-28 08:09:45 +0000

    task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 0. Basic dictionary """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache that inherits from BaseCaching
+    and is a caching system
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if (key or item) is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
