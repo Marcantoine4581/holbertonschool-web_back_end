41838aa058dc339008bf4d454f255b02c2c977bf
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-28 14:46:56 +0000

    task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -17,7 +17,7 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache and discard the last item put in cache
+        """ Add an item in the cache and discard the least recently used item put in cache
         if the number of items in self.cache_data is higher
         that BaseCaching.MAX_ITEMS
         """
