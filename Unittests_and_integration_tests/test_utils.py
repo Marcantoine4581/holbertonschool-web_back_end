416aa7f64628bf83dc34f5445164a4990a0be4f0
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-29 09:17:54 +0000

    annotations added

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -7,10 +7,12 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing the method access_nested_map """
     @parameterized.expand([
        ({"a": 1}, ("a",), 1),
        ({"a": {"b": 2}}, ("a",), {'b': 2}),
        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """  Test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected)
