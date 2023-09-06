7ccec0811a00e7d39f26b9c35f3ded87feeba414
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-06 14:32:15 +0000

    Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     bytes = password.encode('utf-8')  # converting password to array of bytes
     hashed = bcrypt.hashpw(bytes, bcrypt.gensalt())  # Hashing the password
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check valid password """
+    bytes = password.encode('utf-8')
+    return bcrypt.checkpw(bytes, hashed_password)
