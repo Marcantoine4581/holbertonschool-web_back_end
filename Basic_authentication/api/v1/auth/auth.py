ac7f001d13dde0a485e7bce4b6458b6541ad86fd
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-07 09:40:19 +0000

    Define which routes does not need authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -27,7 +27,10 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """ Authorization_header """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current_user """
