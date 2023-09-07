65f966d1d5b8b92970fa9d481f457165e1cf1fdc
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-07 07:59:59 +0000

    Documentation added

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ from typing import List, TypeVar
 
 
 class Auth():
+    """ Class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if a path requires an authentication """
         return False
