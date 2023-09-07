6389e49672ed212d8ba7c50801daee0758038e2c
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-07 08:33:50 +0000

    method require_auth updated

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,22 @@ from typing import List, TypeVar
 class Auth():
     """ Class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if a path requires an authentication """
-        return False
+        """ Checks if a path requires an authentication
+        Returns:
+            True if path is None
+            True if excluded_paths is None or empty
+            False if path is in excluded_paths
+        """
+        if path is None:
+            return True
+        if excluded_paths == [] or excluded_paths is None:
+            return True
+        if path[-1] != "/":
+            path = path + "/"
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization_header """
