778d117a7fb2114f1f10d36a34397a7620740b3f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-12 11:31:23 +0000

    lint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 // curl localhost:1245 && echo ""
 const http = require('http');
 
-port = 1245
+const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
