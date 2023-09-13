af84263f6fda2774030000786b743b19ad470e30
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-13 13:50:26 +0000

    server and routes added

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -25,6 +25,25 @@ class StudentsController {
         response.status(500).send('Cannot load the database');
       });
   }
+
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+    }
+    readDatabase(database)
+      .then((contents) => {
+        const LIST_OF_FIRSTNAMES = contents[major].join(', ');
+        const message = `List: ${LIST_OF_FIRSTNAMES}`;
+
+        response.set('Content-Type', 'text/plain');
+        response.status(200)
+          .send(message);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
 }
 
 module.exports = StudentsController;
---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+const PORT = 1245;
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
