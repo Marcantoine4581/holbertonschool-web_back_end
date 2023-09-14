7622de07ec4dac482d6cdfdd6c6adc302ee36866
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-14 14:18:01 +0000

    test with Stub

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,20 +1,20 @@
 const Utils = require('./utils')
-const sendPaymentRequestToApi = require('./3-payment')
+const sendPaymentRequestToApi = require('./4-payment')
 var expect = require('chai').expect
 var sinon = require("sinon");
 
 describe('sendPaymentRequestToApi', function () {
   it('stub the Utils.calculateNumber function', function () {
-    const stub = sinon.stub(Utils, 'calculateNumber');
-    stub.returns(10);
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');
+    stubUtils.returns(10);
 
     const spyConsole = sinon.spy(console, 'log');
-    resultPrinted = sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
 
-    expect(stub.alwaysCalledWithExactly('SUM', 100, 20)).to.be.true;
+    expect(stubUtils.alwaysCalledWithExactly('SUM', 100, 20)).to.be.true;
     expect(spyConsole.calledWithExactly('The total is: 10')).to.be.true;
 
-    stub.restore();
+    stubUtils.restore();
     spyConsole.restore();
   });
 });
