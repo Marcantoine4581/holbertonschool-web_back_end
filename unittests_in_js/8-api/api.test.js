2ba2a0fc2c9b2fdb3f051900f57fe80da4dcbd0c
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-15 09:53:39 +0000

    Basic Integration testing

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const request = require('request');
+
+const app = require('./api');
+
+describe('test the payement system API', function () {
+  describe('GET /', function () {
+    const url = 'http://localhost:7865/'
+    it('test status code 200', function (done) {
+      request(url, function (error, response, body) {
+        if (error) {
+            done(error)
+        }
+        expect(response.statusCode).to.equal(200);
+        done()
+      });
+    });
+    it('test the correct message', function (done) {
+        request(url, function (error, response, body) {
+          if (error) {
+              done(error)
+          }
+          expect(body).to.equal('Welcome to the payment system');
+          expect(body).to.be.a('string')
+          done()
+        });
+      });
+  });
+});
